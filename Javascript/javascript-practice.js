b37b96ec863af494f2d22795188123d2bd00e08e
Naimul-Islam <naimuliu@gmail.com>
2024-01-28 02:01:11 +0600

    Javascript array added

---
+++ Javascript/javascript-practice.js
@@ -0,0 +1,9 @@
+// var x= 5;
+// var message;
+// message = x>10? "Bigger than 10": "less or equal to 10" 
+
+// console.log(message)
+// array
+const numbers = [5,10,20,30,40]
+const number2 =[15,30,45]
+console.log(numbers.concat(number2))
